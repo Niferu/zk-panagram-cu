e7302200de110d07c45c1f8329a3ae2e9dc430f8
Ciara Nightingale <ciara.nightingale@sky.com>
2025-06-12 09:48:13 +0100

    add readme

--- contracts/js-scripts/generateProof.ts
+++ contracts/js-scripts/generateProof.ts
@@ -1,34 +1,11 @@
 import { Barretenberg, Fr, UltraHonkBackend } from "@aztec/bb.js";
 import { ethers } from "ethers";
-// @ts-ignore
 import { Noir } from "@noir-lang/noir_js";
-
-// @ts-ignore
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
 
-// This gets you the equivalent of __dirname in an ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const circuitPath = path.resolve(__dirname, '../../circuits/target/panagram.json');
-
+const circuitPath = path.resolve(__dirname, "../../circuits/target/zk_panagram.json");
 const circuit = JSON.parse(fs.readFileSync(circuitPath, 'utf8'));
-
-// export function uint8ArrayToHex(buffer: Uint8Array): string {
-//   const hex: string[] = [];
-
-//   buffer.forEach(function (i) {
-//     let h = i.toString(16);
-//     if (h.length % 2) {
-//       h = "0" + h;
-//     }
-//     hex.push(h);
-//   });
-
-//   return hex.join("");
-// }
 
 export default async function generateProof() {
   // Initialize Barretenberg
